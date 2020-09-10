7543646318cfa1534bdf5be7fee5aca6c5f6b7e9
Boboaca Costel <bobyca2003@yahoo.com>
2020-09-10 19:23:49 +0200

    Add files via upload

---
+++ 9.9k_int/nou.bat
@@ -0,0 +1,10 @@
+echo off
+cls
+set /p UserInputPath= Insert your port where costycnc is connected (example com1)
+rem 115200 new bootloader
+
+mode %UserInputPath% BAUD=9600 PARITY=n DATA=8
+    echo E > %UserInputPath%
+avrdude -p m328p -b 57600 -P %UserInputPath% -c arduino -U flash:w:1.1h_x1_y1_9.6k.hex 
+
+pause 
